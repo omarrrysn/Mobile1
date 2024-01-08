8339f0fbc32bc2599c2602a957efdff943aa75aa
omarrrysn <128867241+omarrrysn@users.noreply.github.com>
2024-01-08 08:19:23 -0800

    Update php.dart

--- lib/login/php.dart
+++ lib/login/php.dart
@@ -1,16 +1,13 @@
 class Api{
-  static const hostConnection = "http://192.168.0.102/budget";
-  static const hostConnectionUser = "$hostConnection/user";
-
-  static const signup="$hostConnection/user/signup.php";
-  static const login="$hostConnection/user/login.php";
-  static const getbalance="$hostConnection/user/getbalance.php";
-  static const updatebalance="$hostConnection/user/updatebalance.php";
+  static const signup="https://omar-ysn-chat.000webhostapp.com/budget/user/signup.php";
+  static const login="https://omar-ysn-chat.000webhostapp.com/budget/user/login.php";
+  static const getbalance="https://omar-ysn-chat.000webhostapp.com/budget/user/getbalance.php";
+  static const updatebalance="https://omar-ysn-chat.000webhostapp.com/budget/user/updatebalance.php";
 
 
 
 
-  static const validateEmail="$hostConnection/user/validate_email.php";
+  static const validateEmail="https://omar-ysn-chat.000webhostapp.com/budget/user/validate_email.php";
 
 
 
